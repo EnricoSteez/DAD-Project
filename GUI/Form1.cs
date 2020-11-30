f6f6d48c67dd7d528f588c0c5267896c8a5db076
Robbe Nysen <r0692880@esat.kuleuven.be>
2020-11-30 09:29:02 +0000

    PM GUI

---
+++ GUI/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        private async void Openfile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text File|*.txt", Multiselect = false })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamReader rd = new StreamReader(ofd.FileName))
+                    {
+                        richTextBox1.Text = await rd.ReadToEndAsync();
+                    }
+                }
+
+
+            }
+        }
+    }
+}
