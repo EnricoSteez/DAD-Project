b70f66929fc13b99b05be56d7a78c6122c63825c
Robbe Nysen <r0692880@esat.kuleuven.be>
2020-12-01 18:50:03 +0000

    GUI next version

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button_woc4 = new ePOSOne.btnProduct.Button_WOC();
+            this.button_woc2 = new ePOSOne.btnProduct.Button_WOC();
             this.SuspendLayout();
             // 
             // button_woc1
@@ -48,10 +49,10 @@
             this.button_woc1.OnHoverTextColor = System.Drawing.SystemColors.HighlightText;
             this.button_woc1.Size = new System.Drawing.Size(201, 65);
             this.button_woc1.TabIndex = 1;
-            this.button_woc1.Text = "Run command";
+            this.button_woc1.Text = "Add";
             this.button_woc1.TextColor = System.Drawing.Color.White;
             this.button_woc1.UseVisualStyleBackColor = true;
-            this.button_woc1.Click += new System.EventHandler(this.button1_Click);
+            this.button_woc1.Click += new System.EventHandler(this.Add_Click);
             // 
             // textBox1
             // 
@@ -60,6 +61,8 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(360, 35);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Click += new System.EventHandler(this.none);
+            this.textBox1.Enter += new System.EventHandler(this.Form1_Load);
             // 
             // richTextBox1
             // 
@@ -68,6 +71,8 @@
             this.richTextBox1.Size = new System.Drawing.Size(360, 296);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.Rtb_TextChanged);
             // 
             // button_woc4
             // 
@@ -76,7 +81,7 @@
             this.button_woc4.FlatAppearance.BorderSize = 0;
             this.button_woc4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_woc4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button_woc4.Location = new System.Drawing.Point(528, 274);
+            this.button_woc4.Location = new System.Drawing.Point(528, 386);
             this.button_woc4.Name = "button_woc4";
             this.button_woc4.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.button_woc4.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
@@ -88,6 +93,25 @@
             this.button_woc4.UseVisualStyleBackColor = true;
             this.button_woc4.Click += new System.EventHandler(this.Openfile_Click);
             // 
+            // button_woc2
+            // 
+            this.button_woc2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.button_woc2.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(30)))), ((int)(((byte)(58)))));
+            this.button_woc2.FlatAppearance.BorderSize = 0;
+            this.button_woc2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_woc2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_woc2.Location = new System.Drawing.Point(528, 178);
+            this.button_woc2.Name = "button_woc2";
+            this.button_woc2.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.button_woc2.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.button_woc2.OnHoverTextColor = System.Drawing.SystemColors.HighlightText;
+            this.button_woc2.Size = new System.Drawing.Size(201, 65);
+            this.button_woc2.TabIndex = 1;
+            this.button_woc2.Text = "Run line";
+            this.button_woc2.TextColor = System.Drawing.Color.White;
+            this.button_woc2.UseVisualStyleBackColor = true;
+            this.button_woc2.Click += new System.EventHandler(this.Runline_Click);
+            // 
             // Form1
             // 
             this.AcceptButton = this.button_woc1;
@@ -95,6 +119,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(30)))), ((int)(((byte)(58)))));
             this.ClientSize = new System.Drawing.Size(800, 495);
+            this.Controls.Add(this.button_woc2);
             this.Controls.Add(this.button_woc4);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
@@ -113,6 +138,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private ePOSOne.btnProduct.Button_WOC button_woc4;
+        private ePOSOne.btnProduct.Button_WOC button_woc2;
     }
 }
 
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -23,10 +23,19 @@ namespace GUI
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Add_Click(object sender, EventArgs e)
         {
+            if (textBox1.Text != "")
+            {
 
+                string Cmnd = textBox1.Text + "\r\n";
+                richTextBox1.AppendText(Cmnd);
+
+                textBox1.Text = String.Empty;
+            }
         }
+
+
        
         private async void Openfile_Click(object sender, EventArgs e)
         {
@@ -36,12 +45,78 @@ namespace GUI
                 {
                     using (StreamReader rd = new StreamReader(ofd.FileName))
                     {
-                        richTextBox1.Text = await rd.ReadToEndAsync();
+                        richTextBox1.AppendText(await rd.ReadToEndAsync());
                     }
                 }
 
 
             }
+        }
+        int numlines = 0;
+        private void Rtb_TextChanged(object sender, EventArgs e)
+        {
+            if (newline_index == numlines && newline_index != 0)
+            {
+                var start1 = richTextBox1.GetFirstCharIndexFromLine(newline_index);  // Get the 1st char index of the appended text
+                var length1 = richTextBox1.Lines[newline_index].Length;
+                richTextBox1.Select(start1, length1);
+                richTextBox1.SelectionBackColor = Color.FromArgb(94, 148, 255);
+            }
+            
+            numlines = richTextBox1.Lines.Count()-1;
+            //System.Diagnostics.Debug.WriteLine("nmblines =" + numlines);
+
+            if (numlines == 1)
+            {
+                var start1 = richTextBox1.GetFirstCharIndexFromLine(0);  // Get the 1st char index of the appended text
+                var length1 = richTextBox1.Lines[0].Length;
+                richTextBox1.Select(start1, length1);
+                richTextBox1.SelectionBackColor = Color.FromArgb(94, 148, 255);
+            }
+
+        }
+
+        int line_index = 0;
+        int newline_index = 0;
+        private void Runline_Click(object sender, EventArgs e)
+        {
+            line_index = newline_index;
+            if (line_index + 1 <= numlines)
+            {
+                System.Diagnostics.Debug.WriteLine("nmblines =" + numlines + "  " + "line =" + line_index);
+                //System.Diagnostics.Debug.WriteLine(line);
+                string command = richTextBox1.Lines[line_index];
+                //System.Diagnostics.Debug.WriteLine(command);
+
+                // Unhighlight the executed command and make bold 
+                var start1 = richTextBox1.GetFirstCharIndexFromLine(line_index);  // Get the 1st char index of the appended text
+                var length1 = command.Length;
+                richTextBox1.Select(start1, length1);
+
+                Font font = new Font(richTextBox1.Font.FontFamily, richTextBox1.Font.Size, FontStyle.Bold);
+                richTextBox1.SelectionFont = font; // Bold
+                richTextBox1.SelectionBackColor = Color.Transparent; // Unhighlight
+
+
+                newline_index = line_index + 1;
+                // SEND COMMAND TO PUPPETMASTER
+
+
+                // Highlight the next command
+                if (newline_index < numlines) // so if we not finished all the lines
+                {
+                    var start = richTextBox1.GetFirstCharIndexFromLine(newline_index);  // Get the 1st char index of the appended text
+                    var length = richTextBox1.Lines[newline_index].Length;
+                    richTextBox1.Select(start, length);
+                    richTextBox1.SelectionBackColor = Color.FromArgb(94, 148, 255);
+                }
+            }
+        }
+
+
+        private void none(object sender, EventArgs e)
+        {
+
         }
     }
 }
